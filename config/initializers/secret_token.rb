e644f0a052d2474853a58f5ad4ef6e5901b0c509
Lixiang Jiang <ljian001@fiu.edu>
2013-10-12 12:04:24 -0700

    Create App First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppointmentServices::Application.config.secret_token = '83733ee55c0ac6629150ad77bfba3415e8ff7f904728946ce2b7cd946342b5de707593ad159a9e39b94992e4c16abfc6380aef44a05d0b8f6b20f043d5c0ce6d'
